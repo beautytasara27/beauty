6b886955a69315ac8f0b43e9bff521994b1015ce
beautytasara27 <43332045+beautytasara27@users.noreply.github.com>
2018-10-12 20:33:14 +0700

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,15 +1,49 @@
 class Settings():
+    """ A class to store all settings for Alien Invasion. """
 
     def __init__(self):
+        """ Initialize the game's settings. """
+        # Screen settings
         self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (0, 230, 230)
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
+        #alien settings
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 50
+        #how quickly the game speeds up
+        self.speedup_scale = 1.1
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+        #fleet drxn of 1 represents right; -1 represents left
+        self.fleet_direction = 1
+
+        # Ship settings
         self.ship_speed_factor = 1.5
-        self.bullet_speed_factor = 1
+        self.ship_limit = 3
+
+        # Bullet settings
+        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bulleet_color = 60, 60, 60
+        self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
+    def initialize_dynamic_settings(self):
+        """initialize settings that change throughout the game """
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 5
+        self.alien_speed_factor = 1
 
+        #fleet direction of 1 represents right; -1 represents left
+        self.fleet_direction = 1
+        #scoring
+        self.alien_points = 50
 
+    def increase_speed(self):
+        """increase speed settings and alien point values"""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
